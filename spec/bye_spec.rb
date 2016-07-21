e5865c0daa7258298ddea93c6bb0cace2483ad88
Chris Woodrich <cwoodrich@doximity.com>
2016-07-20 20:53:14 -0700

    "tests"

--- spec/bye_spec.rb
+++ spec/bye_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 
 describe Bye do
-  it 'has a version number' do
-    expect(Bye::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(Bye).to eq(true)
+  describe Object do
+    it 'does not have a try method' do
+      expect(Object.methods).to_not include(:try)
+    end
   end
 end
